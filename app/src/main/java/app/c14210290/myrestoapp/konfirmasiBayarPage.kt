d8222bc29590911b42ac0bfb7bd2ba21c4475342
Theodorus Dustin <154519546+c14210223@users.noreply.github.com>
2024-12-25 08:33:48 +0700

    Maaf lupa push

--- app/src/main/java/app/c14210290/myrestoapp/konfirmasiBayarPage.kt
+++ app/src/main/java/app/c14210290/myrestoapp/konfirmasiBayarPage.kt
@@ -1,29 +1,20 @@
 package app.c14210290.myrestoapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class kasirWaiter_page : AppCompatActivity() {
+class konfirmasiBayarPage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_kasir_waiter_page)
+        setContentView(R.layout.activity_konfirmasi_bayar_page)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val _btnTransaction = findViewById<Button>(R.id.btn_transaction)
-        _btnTransaction
-        _btnTransaction.setOnClickListener {
-            val intent = Intent(this, konfirmasiBayarPage::class.java)
-            startActivity(intent)
         }
     }
 }
